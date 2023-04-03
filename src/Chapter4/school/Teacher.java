ee370ec44642f797fd1c22d3bcc033e525ed94ea
ryanscoles <ryanscoles@gmail.com>
2023-04-03 16:38:51 -0500

    first commit

---
+++ src/Chapter4/school/Teacher.java
@@ -0,0 +1,16 @@
+package Chapter4.school;
+
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private int yearsTeaching;
+
+    public String getFirstName() {return this.firstName;}
+    private void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return this.lastName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public int getYearsTeaching() {return this.yearsTeaching;}
+    public void setYearsTeaching(int yearsTeaching) {this.yearsTeaching = yearsTeaching;}
+}
